96900cd889a86a63aee93dbc5611f43f842fc9ea
Matthew Smit <programmdude@gmail.com>
2019-09-17 00:51:30 +1200

    Fixed windows compile.

--- CPVulkan/SwapchainWin32.cpp
+++ CPVulkan/SwapchainWin32.cpp
@@ -1,6 +1,8 @@
 #include "Swapchain.h"
 
 #include "Image.h"
+#include "Instance.h"
+#include "PhysicalDevice.h"
 
 #include <Windows.h>
 #include <vulkan/vk_icd.h>
@@ -58,50 +60,50 @@ VkResult Swapchain::Present(uint32_t pImageIndex)
 
 void Instance::DestroySurface(VkSurfaceKHR surface, const VkAllocationCallbacks* pAllocator)
 {
-    Free(reinterpret_cast<VkIcdSurfaceWin32*>(surface), pAllocator);
+	Free(reinterpret_cast<VkIcdSurfaceWin32*>(surface), pAllocator);
 }
 
 VkResult Instance::CreateWin32Surface(const VkWin32SurfaceCreateInfoKHR* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSurfaceKHR* pSurface)
 {
-    assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR);
-    assert(pCreateInfo->pNext == nullptr);
-    assert(pCreateInfo->flags == 0);
+	assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR);
+	assert(pCreateInfo->pNext == nullptr);
+	assert(pCreateInfo->flags == 0);
 
-    auto surface = Allocate<VkIcdSurfaceWin32>(pAllocator, VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE);
-    surface->base.platform = VK_ICD_WSI_PLATFORM_WIN32;
-    surface->hinstance = pCreateInfo->hinstance;
-    surface->hwnd = pCreateInfo->hwnd;
-    *pSurface = reinterpret_cast<VkSurfaceKHR>(surface);
-    return VK_SUCCESS;
+	auto surface = Allocate<VkIcdSurfaceWin32>(pAllocator, VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE);
+	surface->base.platform = VK_ICD_WSI_PLATFORM_WIN32;
+	surface->hinstance = pCreateInfo->hinstance;
+	surface->hwnd = pCreateInfo->hwnd;
+	*pSurface = reinterpret_cast<VkSurfaceKHR>(surface);
+	return VK_SUCCESS;
 }
 
 VkResult PhysicalDevice::GetSurfaceCapabilities(VkSurfaceKHR surface, VkSurfaceCapabilitiesKHR* pSurfaceCapabilities)
 {
-    const auto win32Surface = reinterpret_cast<VkIcdSurfaceWin32*>(surface);
-    RECT rect;
-    assert(GetClientRect(win32Surface->hwnd, &rect));
+	const auto win32Surface = reinterpret_cast<VkIcdSurfaceWin32*>(surface);
+	RECT rect;
+	assert(GetClientRect(win32Surface->hwnd, &rect));
 
-    pSurfaceCapabilities->minImageCount = 1;
-    pSurfaceCapabilities->maxImageCount = 0;
-    pSurfaceCapabilities->currentExtent.width = rect.right - rect.left;
-    pSurfaceCapabilities->currentExtent.height = rect.bottom - rect.top;
-    pSurfaceCapabilities->minImageExtent.width = 1;
-    pSurfaceCapabilities->minImageExtent.height = 1;
-    pSurfaceCapabilities->maxImageExtent.width = 8192;
-    pSurfaceCapabilities->maxImageExtent.height = 8192;
-    pSurfaceCapabilities->maxImageArrayLayers = 1;
-    pSurfaceCapabilities->supportedTransforms = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
-    pSurfaceCapabilities->currentTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
-    pSurfaceCapabilities->supportedCompositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-    pSurfaceCapabilities->supportedUsageFlags = VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
-                                                VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-                                                VK_IMAGE_USAGE_SAMPLED_BIT |
-                                                VK_IMAGE_USAGE_STORAGE_BIT |
-                                                VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
-                                                VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT |
-                                                VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT |
-                                                VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT |
-                                                VK_IMAGE_USAGE_SHADING_RATE_IMAGE_BIT_NV |
-                                                VK_IMAGE_USAGE_FRAGMENT_DENSITY_MAP_BIT_EXT;
-    return VK_SUCCESS;
+	pSurfaceCapabilities->minImageCount = 1;
+	pSurfaceCapabilities->maxImageCount = 0;
+	pSurfaceCapabilities->currentExtent.width = rect.right - rect.left;
+	pSurfaceCapabilities->currentExtent.height = rect.bottom - rect.top;
+	pSurfaceCapabilities->minImageExtent.width = 1;
+	pSurfaceCapabilities->minImageExtent.height = 1;
+	pSurfaceCapabilities->maxImageExtent.width = 8192;
+	pSurfaceCapabilities->maxImageExtent.height = 8192;
+	pSurfaceCapabilities->maxImageArrayLayers = 1;
+	pSurfaceCapabilities->supportedTransforms = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+	pSurfaceCapabilities->currentTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+	pSurfaceCapabilities->supportedCompositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	pSurfaceCapabilities->supportedUsageFlags = VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+		VK_IMAGE_USAGE_TRANSFER_DST_BIT |
+		VK_IMAGE_USAGE_SAMPLED_BIT |
+		VK_IMAGE_USAGE_STORAGE_BIT |
+		VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
+		VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT |
+		VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT |
+		VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT |
+		VK_IMAGE_USAGE_SHADING_RATE_IMAGE_BIT_NV |
+		VK_IMAGE_USAGE_FRAGMENT_DENSITY_MAP_BIT_EXT;
+	return VK_SUCCESS;
 }
